9c450cc4eafe980dc51d7783373ec35864ec0c46
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-15 13:46:41 +0700

    feat(project): 04 tipe data string

---
+++ 04_string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Manuel ")
+	fmt.Println("Manuel Theodore ")
+	fmt.Println("Manuel Theodore Leleuly")
+
+	fmt.Println(len("Manuel "))
+	fmt.Println("Manuel Theodore "[0])
+	fmt.Println("Manuel Theodore Leleuly"[1])
+}
