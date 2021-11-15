83348e0feaa4f853e6bc4517969ad65906ed340a
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-15 14:39:05 +0700

    feat(project): 10 comparations

---
+++ 10_comparations.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	name1 := "Manuel"
+	name2 := "Manuel"
+
+	result := name1 == name2
+	fmt.Println(result)
+
+	value1 := 100
+	value2 := 200
+
+	fmt.Println(value1 > value2)
+}
