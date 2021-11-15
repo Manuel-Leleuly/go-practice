98c3e25020f8433d8e7a9372fdb342123bcf4c96
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-15 14:10:05 +0700

    feat(project): create constant

---
+++ 06_constant.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const firstName string = "Manuel"
+	// const lastName = "Leleuly"
+	// const value = 1000
+
+	const (
+		firstName string = "Manuel"
+		lastName = "Leleuly"
+		value = 1000
+	)
+
+	fmt.Println(firstName)
+}
