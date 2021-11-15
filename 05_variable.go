8bb59eecf8bc973a64ade69a3bc8dee80851db7c
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-15 14:03:32 +0700

    feat(project): create variable

---
+++ 05_variable.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string
+
+	name = "Manuel Theodore"
+	fmt.Println(name)
+
+	name = "Manuel Leleuly"
+	fmt.Println(name)
+
+	var name2 = "Manuel Theodore"
+	fmt.Println(name2)
+
+	var age int8 = 26
+	fmt.Println(age)
+
+	country := "indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Manuel"
+		lastName = "Leleuly"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
