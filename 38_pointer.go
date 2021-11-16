dfc8483fa6211a2c97b0d0c8775103d0ab72b62c
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-16 15:05:18 +0700

    feat(project): pointer

---
+++ 38_pointer.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Address struct {
+	City, Province, Country string
+}
+
+func main() {
+	address1 := Address{"Subang", "Jawa Barat", "Indonesia"}
+	address2 := &address1
+
+	address2.City = "Bandung"
+
+	*address2 = Address{"Malang", "Jawa Timur", "Indonesia"}
+
+	fmt.Println(address1)
+	fmt.Println(*address2)
+
+	address4 := new(Address)
+	address4.City = "Jakarta"
+	fmt.Println(address4)
+}
