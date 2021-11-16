e7f396a35c362bfbcfba15142f0044f9f909502d
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-16 11:20:03 +0700

    feat(project): create a function that uses a function as the parameter

---
+++ 26_functionAsParameter.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Filter func(string) string
+
+func sayHelloWithFilter(name string, filter Filter) {
+	fmt.Println("Hello", filter(name))
+}
+
+func spamFilter(name string) string {
+	if name == "Anjing"{
+		return "..."
+	}
+	return name
+}
+
+func main() {
+	sayHelloWithFilter("Manuel", spamFilter)
+	sayHelloWithFilter("Anjing", spamFilter)
+}
