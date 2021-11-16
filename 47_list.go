9a811f8c879af7aee0bb1b456cf53a57d0dbf176
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-16 19:53:50 +0700

    feat(project): container list

---
+++ 47_list.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+func main() {
+	data := list.New()
+
+	data.PushBack("Manuel")
+	data.PushBack("Theodore")
+	data.PushBack("Leleuly")
+
+	fmt.Println(data.Front().Value)
+
+	// dari depan ke belakang
+	for element := data.Front(); element != nil; element = element.Next(){
+		fmt.Println(element.Value)
+	}
+
+	// dari belakang ke depan
+	for element := data.Back(); element != nil; element = element.Prev(){
+		fmt.Println(element.Value)
+	}
+}
