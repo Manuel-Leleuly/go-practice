e55008770c73fe9ff21626ba752db8707da4d34d
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-20 21:20:54 +0700

    feat(goroutine): create channel

---
+++ 04_goroutines/channel_test.go
@@ -0,0 +1,23 @@
+package belajargolanggoroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Manuel Theodore Leleuly"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
