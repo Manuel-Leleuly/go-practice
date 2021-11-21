b4fd8664d2035390e51485b5ca9cd123d809a123
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-21 10:57:34 +0700

    feat(goroutine): create a ranged channel

--- 04_goroutines/goroutine_test.go
+++ 04_goroutines/goroutine_test.go
@@ -2,6 +2,7 @@ package belajargolanggoroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -96,5 +97,26 @@ func TestBufferedChannel(t *testing.T) {
 	}()
 
 	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
+
+/*
+	If you are not sure how many times the channel will receive a data, you can use a ranged channel using for loop
+*/
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+
 	fmt.Println("Selesai")
 }
